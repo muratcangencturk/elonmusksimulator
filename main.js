d8b7a0a1b2813b79c94ee9068a2bdb0a5c821aeb
muratcangencturk <muratcangencturk@windowslive.com>
2025-06-13 14:50:58 +0300

    Update service worker registration

--- main.js
+++ main.js
@@ -746,7 +746,7 @@ window.addEventListener('load', function() {
     initI18n();
     initGame();
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('sw.js')
+        navigator.serviceWorker.register('sw.js', { updateViaCache: 'none' })
             .then(reg => console.log('Service Worker registered', reg))
             .catch(err => console.error('Service Worker registration failed:', err));
     }
